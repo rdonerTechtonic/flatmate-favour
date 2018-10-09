cef58b4a4582d1148f11d96c57d1b54462226130
Ryan Doner <>
2018-10-09 12:01:24 -0600

    create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function Event(obj){
+  this.eventTitle = obj.eventTitle;
+  this.eventLocation = obj.eventLocation;
+  this.eventRoommates = obj.eventRoommates;
+  this.eventNotes = obj.eventNotes;
+  this.eventStartDate = obj.eventStartDate;
+  this.eventStartDate = obj.eventStartDate;
+  this.eventEndDate = obj.eventEndDate;
+  this.eventEndTime = obj.eventEndTime;
+}
+
+function House(obj){
+  this.HouseName: obj.HouseName
+  this.HouseRoomates: obj.HouseRoomates
+}
