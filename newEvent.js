f0ce3f527de166e741cb2c0d982256f4d93a9e47
Devin Giannoni <>
2018-10-10 11:41:54 -0600

    Created a function to update the 'selectRoommate' multi select with the contents of the current house object.  This should be called anytime the New/Edit pages are loaded, or if a roommate is Added/Removed.

--- newEvent.js
+++ newEvent.js
@@ -24,11 +24,13 @@ flatMateFavour.prototype.grabNewEventData = function (eventPoster) {
   return newEvent;
 };
 
-//$('#selectRoommate').append('<option value="6">Six</option>')
 
-flatMateFavour.prototype.populateRoommateSelector = function () {
-  for (var i = 0; i < event.eventRoommates.length; i++) {
-    var roommateSelect= testHousehold.HouseRoommates[i];
-    console.log(testHousehold.HouseRoommates[i]);
+//This is a function to populate the New/Edit event and New/Edit household
+flatMateFavour.prototype.updateRoommateSelector = function () {
+  $('#selectRoommate').children().remove()
+  for (var i = 0; i < testHousehold.ffHouse.HouseRoomates.length; i++) {
+    var roommateSelect = testHousehold.ffHouse.HouseRoomates[i];
+      var appendString = '<option value="' + i + '">' + testHousehold.ffHouse.HouseRoomates[i] + '</option>'
+      $('#selectRoommate').append(appendString);
   }
 }
