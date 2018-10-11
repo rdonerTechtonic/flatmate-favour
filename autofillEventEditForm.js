5e0819612b9ed4e6575509a858fff9390009bf28
Delaney Hanson <delaney.hanson@techtonic.com>
2018-10-11 09:22:15 -0600

    renames the autofillEventEditForm and changes it in the event html as well as adds the populateDashboardWithRoommates js file

---
+++ autofillEventEditForm.js
@@ -0,0 +1,32 @@
+//  Function that populates event edit form with event object
+//I want to get the information from the specified event object
+//I want to then take each value of each info object and populate the correct part of the form with the corresponding info object
+
+//function that is called when button is pressed
+//function that populates edit form with information
+function autoFill (eventToFillWith) {
+  $('#eventTitle').val(eventToFillWith.eventTitle);
+  $('#eventLocation').val(eventToFillWith.eventLocation);
+  $('#eventNotes').val(eventToFillWith.eventNotes);
+
+  var splitStartDate = eventToFillWith.eventStartDate.split('T');
+  var splitEndDate = eventToFillWith.eventEndDate.split('T');
+
+  var onlyStartDate = splitStartDate[0];
+  var onlyEndDate = splitEndDate[0];
+
+  var splitStartTime = splitStartDate[1].split('.');
+  var splitEndTime = splitEndDate[1].split('.');
+
+  var onlyStartTime = splitStartTime[0];
+  var onlyEndTime = splitEndTime[0];
+
+  $('#startEventDate').val(onlyStartDate);
+  $('#startEventTime').val(onlyStartTime);
+  $('#endEventDate').val(onlyEndDate);
+  $('#endEventTime').val(onlyEndTime);
+};
+
+
+//test case:
+// autoFill(testHousehold.ffEvents[1])
