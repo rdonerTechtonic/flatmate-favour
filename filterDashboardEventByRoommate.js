6028794c19235f5f2f091b8aa783e799d150888e
Delaney Hanson <delaney.hanson@techtonic.com>
2018-10-11 11:42:38 -0600

    init commit

--- filterDashboardEventByRoommate.js
+++ filterDashboardEventByRoommate.js
@@ -1,3 +1,10 @@
+var filteredResults = [];
+
 function filterDashboardEventByRoommate () {
-  
+  $('div.dashboardUsers > button').on('click', function () {
+    var filteredResults = [];
+    for (var i = 0; i < testHousehold.ffHouse.HouseRoomates.length; i++) {
+      filteredResults.push()
+    };
+  });
 };
