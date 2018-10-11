f7f577e1bd59020d017c9a84a77d2f1069ee9e4f
Delaney Hanson <delaney.hanson@techtonic.com>
2018-10-11 15:11:59 -0600

    adds js file to dashboard and attempts to fix filter function

--- filterDashboardEventByRoommate.js
+++ filterDashboardEventByRoommate.js
@@ -1,15 +1,33 @@
-var filteredResults = [];
+$(document).ready(filter);
 
-function filterDashboardEventByRoommate () {
-  $('div.dashboardUsers > button').on('click', function () {
-    var filteredResults = [];
-    for (var i = 0; i < testHousehold.ffHouse.HouseRoomates.length; i++) {
-      for (var x = 0; x < testHousehold.ffEvents.length; x++) {
-        if (testHousehold.ffHouse.HouseRoomates[i] === testHousehold.ffEvents[x].eventPostedBy){
-          filteredResults.push(testHousehold.ffEvents[x])
-        };
-      };
+function filter () {
+  $('div.dashboardUsers > button').on('click', function filterDashboardEventByRoommate () {
+    var filteredEvents = [];
+    for (var x = 0; x < testHousehold.ffEvents.length; x++) {
+      console.log(testHousehold.ffEvents[x].eventPostedBy);
+      // var splitPostedBy = testHousehold.ffEvents[x].eventPostedBy.split("@");
+      // var readablePostedBy = splitPostedBy[0];
+      // console.log(splitPostedBy);
+      // if (buttonPushedID === readablePostedBy){
+      //   filteredEvents.push(testHousehold.ffEvents[x])
+      // };
     };
-    $('#accordion').append('')
+    // console.log(filteredEvents);
+    // $('#accordion').append(filteredEvents);
   });
 };
+
+
+//filter approach
+
+// function checkForUserMatch (buttonID) {
+//   for (var x = 0; x < testHousehold.ffEvents.length; x++) {
+//     var splitPostedBy = testHousehold.ffEvents[x].eventPostedBy.split("@");
+//     var readablePostedBy = splitPostedBy[0];
+//     return buttonID === readablePostedBy;
+//   };
+// };
+//
+// function filterEvents () {
+//
+// };
