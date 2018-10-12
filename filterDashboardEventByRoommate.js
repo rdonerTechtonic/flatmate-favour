f104c8cff183b9a9638de129dd8b515cca1d710d
Delaney Hanson <delaney.hanson@techtonic.com>
2018-10-12 09:00:13 -0600

    fixes syntax error on filter

--- filterDashboardEventByRoommate.js
+++ filterDashboardEventByRoommate.js
@@ -1,41 +1,16 @@
 $(document).ready(filter);
 
 function filter () {
-  // $('div.dashboardUsers > button').on('click', function filterDashboardEventByRoommate () {
+  $("div.dashboardUsers > button").on('click', function filterDashboardEventByRoommate() {
+    $('#accordion').children().remove();
     var filteredEvents = [];
-    $("div.dashboardUsers > button").each(function(){
-      this.on('click', function filterDashboardEventByRoommate() {
-        var filteredEvents = [];
-    // for (var x = 0; x < testHousehold.ffEvents.length; x++) {
-      // console.log(testHousehold.ffEvents[x].eventPostedBy);
-      // var splitPostedBy = testHousehold.ffEvents[x].eventPostedBy.split("@");
-      // var readablePostedBy = splitPostedBy[0];
-      // console.log(splitPostedBy);
-
-      // attr('id'));
-      // console.log($("div.dashboardUsers button").attr('id'));
-      // if (buttonPushedID === readablePostedBy){
-      //   filteredEvents.push(testHousehold.ffEvents[x])
-      // };
-    // });
-    // console.log(filteredEvents);
-    // $('#accordion').append(filteredEvents);
-    // console.log('filterDashboardEventByRoommate has been run');
-  // };
-});
-}
-
-
-//filter approach
-
-// function checkForUserMatch (buttonID) {
-//   for (var x = 0; x < testHousehold.ffEvents.length; x++) {
-//     var splitPostedBy = testHousehold.ffEvents[x].eventPostedBy.split("@");
-//     var readablePostedBy = splitPostedBy[0];
-//     return buttonID === readablePostedBy;
-//   };
-// };
-//
-// function filterEvents () {
-//
-// };
+    console.log(filteredEvents);
+    var buttonID = $("div.dashboardUsers > button").attr('id');
+    for (var i = 0; i < testHousehold.ffEvents.length; i++) {
+      if(buttonID == testHousehold.ffEvents[i].eventPostedBy) {
+        filteredEvents.push(testHousehold.ffEvents[i]);
+      };
+    };
+    $('#accordion').append(filteredEvents);
+  });
+};
