1684eacaca02ee4e3d5686c44933fcd05f578f21
Delaney Hanson <delaney.hanson@techtonic.com>
2018-10-12 10:20:19 -0600

    makes it so the user icons are inline elements in dashboard.html, updates jquery selector to match in populateDashboardWithRoommates, and edits the theme.css to make the buttons look nicer

--- populateDashboardWithRoommates.js
+++ populateDashboardWithRoommates.js
@@ -2,7 +2,7 @@ $(document).ready(populateDashboardWithRoommates);
 
 function populateDashboardWithRoommates () {
   for (var i = 0; i < testHousehold.ffHouse.HouseRoomates.length; i++) {
-    $('div.dashboardUsers').append('<button id="' + testHousehold.ffHouse.HouseRoomates[i] + '"<a href="#"><i class="fas fa-user"></i></a></button>');
+    $('span.dashboardUsers').append('<button class="btn btn-info" id="' + testHousehold.ffHouse.HouseRoomates[i] + '"<a href="#"><i class="fas fa-user"></i></a></button>');
     // console.log(testHousehold.ffHouse);
   };
 };
